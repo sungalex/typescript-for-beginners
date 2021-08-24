8c67f6a93cbb68ea450b651b6d4ab29dbab82318
sungalex <cloudallera@gmail.com>
2021-08-25 03:02:29 +0900

    Classes on Typescript - part One

--- README.md
+++ README.md
@@ -148,3 +148,38 @@ const sayHi = (person: Human): string => {
 
 console.log(sayHi(person));
 ```
+
+- `ts`의 `interface`는 `js`로 컴파일 되지는 않음
+
+## 4. Classes on Typescript - part One
+
+```typescript
+/**
+ * js에서는 class의 속성을 선언할 필요가 없지만,
+ * ts에서는 class의 속성(Property)과 Permission을 선언해야 함
+ */
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+// person Object(instance) 생성
+const alex = new Human('Alex', 54, 'male');
+
+/**
+ * 변수명과 함수명에 Type을 지정하여 ts가 자동으로 오류를 체크하도록 함
+ * 함수의 파라미터 Type을 Human class로 지정하고,
+ * 함수 호출 시 Human class의 instance를 인수로 전달
+ */
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are ${person.gender}!`;
+};
+
+console.log(sayHi(alex));
+```
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,31 @@
 /**
- * interface 선언 (interface에 포함된 속성 이름과 속성별 Type 지정)
+ * js에서는 class의 속성을 선언할 필요가 없지만,
+ * ts에서는 class의 속성(Property)과 Permission을 선언해야 함
  */
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-// person Object 생성
-const person = {
-  name: 'Alex',
-  age: 54,
-  gender: 'male',
-};
+// person Object(instance) 생성
+const alex = new Human('Alex', 54, 'male');
 
 /**
  * 변수명과 함수명에 Type을 지정하여 ts가 자동으로 오류를 체크하도록 함
- * 함수의 파라미터 Type을 Human interface로 지정하고,
- * 함수 호출 시 Human interface와 동일한 속성을 가진 Object를 인수로 전달
+ * 함수의 파라미터 Type을 Human class로 지정하고,
+ * 함수 호출 시 Human class의 instance를 인수로 전달
  */
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(alex));
 
 /**
  * ECMAScript 2015와 마찬가지로 TypeScript는 최상위 수준의 import 혹은 export가 포함된 모든 파일을 모듈로 간주합니다.
